b22c41634759c0325e17e34af893b72b1f89ac7f
nathalia oliver <naty2005oliverr@gmail.com>
2026-02-24 22:12:32 -0300

    Aula 1 - Variáveis em JavaScript

---
+++ src/script.js
@@ -0,0 +1,15 @@
+// Exemplos de variável let
+
+let nome = 'Nathalia';
+
+let nome2 = 'Eduardo';
+
+console.log(nome);
+console.log(nome2);
+
+//Exemplos de variável const
+
+const times = ['São Paulo', 'Flamengo', 'Santos'];
+
+console.log(times);
+
