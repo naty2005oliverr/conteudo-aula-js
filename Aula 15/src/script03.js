88a0fa4e95d7c33373d414742b8a40788f0ee9c6
nathalia oliver <naty2005oliverr@gmail.com>
2026-03-23 14:10:15 -0300

    Estudando Manipulações de Elementos no HTML

---
+++ Aula 15/src/script03.js
@@ -0,0 +1,7 @@
+// Utilizando o innerHTML
+document.querySelector(".mudandoElemento").innerHTML = "Nathalia Oliveira";
+
+// Alterando atributos
+const elemento = document.querySelector(".foto");
+
+elemento.src = "image.png";
